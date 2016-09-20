c7cf35ab31f47a8a93d9c16b387c80d808b7a833
unknown <TungNg@VNDEV18.augensoftwaregroup.com.vn>
2016-09-20 17:06:22 +0700

    design index form

--- resources/views/front/template.blade.php
+++ resources/views/front/template.blade.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
-
     <head>
-
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>{{ trans('front/site.title') }}</title>
@@ -15,119 +13,130 @@
         @yield('head')
 
         {!! HTML::style('css/front.css') !!}
-
+        {!! HTML::style('css/front_style.css') !!}
     </head>
-
   <body>
-
-    <header>
-
-        <div class="brand">{{ trans('front/site.title') }}</div>
-        <div class="address-bar">{{ trans('front/site.sub-title') }}</div>
-        <div id="flags" class="text-center"></div>
-        <nav class="navbar navbar-default">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.html">{{ trans('front/site.title') }}</a>
-                </div>
-                <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav">
-                        <li {!! classActivePath('/') !!}>
-                            {!! link_to('/', trans('front/site.home')) !!}
-                        </li>
-                        @if(session('statut') == 'visitor' || session('statut') == 'user')
-                            <li {!! classActivePath('contact/create') !!}>
-                                {!! link_to('contact/create', trans('front/site.contact')) !!}
-                            </li>
-                        @endif
-                        <li {!! classActiveSegment(1, ['articles', 'blog']) !!}>
-                            {!! link_to('articles', trans('front/site.blog')) !!}
-                        </li>
-                        @if(Request::is('auth/register'))
-                            <li class="active">
-                                {!! link_to('auth/register', trans('front/site.register')) !!}
-                            </li>
-                        @elseif(Request::is('password/email'))
-                            <li class="active">
-                                {!! link_to('password/email', trans('front/site.forget-password')) !!}
-                            </li>
-                        @else
-                            @if(session('statut') == 'visitor')
-                                <li {!! classActivePath('login') !!}>
-                                    {!! link_to('login', trans('front/site.connection')) !!}
-                                </li>
-                            @else
-                                @if(session('statut') == 'admin')
-                                    <li>
-                                        {!! link_to_route('admin', trans('front/site.administration')) !!}
-                                    </li>
-                                @elseif(session('statut') == 'redac') 
-                                    <li>
-                                        {!! link_to('blog', trans('front/site.redaction')) !!}
-                                    </li>
-                                @endif
-                                <li>
-                                    {!! link_to('/logout', trans('front/site.logout'), ['id' => "logout"]) !!}
-                                    {!! Form::open(['url' => '/logout', 'id' => 'logout-form']) !!}{!! Form::close() !!}
-                                </li>
-                            @endif
-                        @endif
-                        <li class="dropdown">
-                            <a data-toggle="dropdown" class="dropdown-toggle" href="#"><img width="32" height="32" alt="{{ session('locale') }}"  src="{!! asset('img/' . session('locale') . '-flag.png') !!}" />&nbsp; <b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-                            @foreach ( config('app.languages') as $user)
-                                @if($user !== config('app.locale'))
-                                    <li><a href="{!! url('language') !!}/{{ $user }}"><img width="32" height="32" alt="{{ $user }}" src="{!! asset('img/' . $user . '-flag.png') !!}"></a></li>
-                                @endif
-                            @endforeach
-                            </ul>
-                        </li>
-                    </ul>
+    <div class="container">
+        <header>
+            <!--http://bootsnipp.com/snippets/featured/expanding-search-button-in-css-->
+            <div class="row">
+                <div class="col-md-4 col-md-offset-3">
+                    <form action="" class="search-form">
+                        <div class="form-group has-feedback">
+                                <label for="search" class="sr-only">Search</label>
+                                <input type="text" class="form-control" name="search" id="search" placeholder="search">
+                                <span class="glyphicon glyphicon-search form-control-feedback"></span>
+                        </div>
+                    </form>
                 </div>
             </div>
-        </nav>
-        @yield('header')    
-    </header>
+            <div id="logo">
+                <a href="/">
+                    <img src="{!! asset('img/logo.png') !!}"alt="Logo">
+                </a>
+            </div>                 
 
-    <main class="container">
-        @if(session()->has('ok'))
-            @include('partials/error', ['type' => 'success', 'message' => session('ok')])
-        @endif  
-        @if(isset($info))
-            @include('partials/error', ['type' => 'info', 'message' => $info])
-        @endif
-        @yield('main')
-    </main>
+            <div class="brand">{{ trans('front/site.title') }}</div>
+            <div class="address-bar">{{ trans('front/site.sub-title') }}</div>
+            <div id="flags" class="text-center"></div>
+            <div id="header-bottom">
+                <nav class="navbar navbar-default pull-left">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="index.html">{{ trans('front/site.title') }}</a>
+                    </div>
+                    <div class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav">
+                            <li {!! classActivePath('/') !!}>
+                                {!! link_to('/', trans('front/site.home')) !!}
+                            </li>
+                            @if(session('statut') == 'visitor' || session('statut') == 'user')
+                                <li {!! classActivePath('contact/create') !!}>
+                                    {!! link_to('contact/create', trans('front/site.contact')) !!}
+                                </li>
+                            @endif
+                            <li {!! classActiveSegment(1, ['articles', 'blog']) !!}>
+                                {!! link_to('articles', trans('front/site.blog')) !!}
+                            </li>
+                            @if(Request::is('auth/register'))
+                                <li class="active">
+                                    {!! link_to('auth/register', trans('front/site.register')) !!}
+                                </li>
+                            @elseif(Request::is('password/email'))
+                                <li class="active">
+                                    {!! link_to('password/email', trans('front/site.forget-password')) !!}
+                                </li>
+                            @else
+                                @if(session('statut') == 'visitor')
+                                    <li {!! classActivePath('login') !!}>
+                                        {!! link_to('login', trans('front/site.connection')) !!}
+                                    </li>
+                                @else
+                                    @if(session('statut') == 'admin')
+                                        <li>
+                                            {!! link_to_route('admin', trans('front/site.administration')) !!}
+                                        </li>
+                                    @elseif(session('statut') == 'redac') 
+                                        <li>
+                                            {!! link_to('blog', trans('front/site.redaction')) !!}
+                                        </li>
+                                    @endif
+                                    <li>
+                                        {!! link_to('/logout', trans('front/site.logout'), ['id' => "logout"]) !!}
+                                        {!! Form::open(['url' => '/logout', 'id' => 'logout-form']) !!}{!! Form::close() !!}
+                                    </li>
+                                @endif
+                            @endif                        
+                        </ul>
+                    </div>
+                </nav>
+                <div id="header-right-bottom" class="pull-right">
+                    <span><i class="fa fa-phone"></i> 0123.456.789</span>
+                    <span><i class="fa fa-commenting-o"></i>&nbsp;CHÁT ONLINE</span> 
+                </div>
+            </div>
+            @yield('header')    
+        </header>
 
-    <footer>
-        @yield('footer')
-        <p class="text-center"><small>Copyright &copy; Momo</small></p>
-    </footer>
-        
-    {!! HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js') !!}
-    {!! HTML::script('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js') !!}
+        <main>
+            @if(session()->has('ok'))
+                @include('partials/error', ['type' => 'success', 'message' => session('ok')])
+            @endif  
+            @if(isset($info))
+                @include('partials/error', ['type' => 'info', 'message' => $info])
+            @endif
+            @yield('main')
+        </main>
 
-    <script>
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-        $(function() {
-            $('#logout').click(function(e) {
-                e.preventDefault();
-                $('#logout-form').submit();
-            })
-        });
-    </script>
+        <footer>
+            @yield('footer')
+            <p class="text-center"><small>Copyright &copy; Momo</small></p>
+        </footer>
 
-    @yield('scripts')
+        {!! HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js') !!}
+        {!! HTML::script('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js') !!}
 
+        <script>
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $(function() {
+                $('#logout').click(function(e) {
+                    e.preventDefault();
+                    $('#logout-form').submit();
+                })
+            });
+        </script>
+
+        @yield('scripts')
+
+    <div class="container">
   </body>
 </html>
