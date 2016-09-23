88fe8d5bac8a992c7dc6187c9ffaefa46fc52c0f
unknown <TungNg@VNDEV18.augensoftwaregroup.com.vn>
2016-09-23 17:03:45 +0700

    add footer & images

--- resources/views/front/template.blade.php
+++ resources/views/front/template.blade.php
@@ -105,7 +105,57 @@
 
         <footer>
             @yield('footer')
-            <p class="text-center"><small>Copyright &copy; Momo</small></p>
+                <div class="row footer-details clearfix">
+                    <div id="footer-company-info" class="col-sm-3">                
+                        <div id="company-info">
+                            <img alt="" src="/file/ckfinder/userfiles/images/logo.png" style="height: 52px; width: 177px">                           
+                        </div>
+                    <hr>
+                        <div id="address-details" style="color: #bababa">
+                            <p class="location">Cao ốc Bùi Đình Tuý, Phường 24, Quận Bình Thạnh.</p>
+                            <p class="phone">Phone: <strong>3-512-8400</strong> ( 3 lines )</p>
+                            <p class="hotline">Hotline: 0123.456.7898</p>
+                            <p class="fax">Fax:&nbsp;<span style="line-height: 20.8px">3-512-8400</span></p>
+                            <p class="mst">MST: 123456789</p>
+                            <p class="email">Email: <a href="mailto:info@emsvn.com">info@emsvn.com</a></p>
+                            <p class="website">Website: <a href="mailto:info@emsvn.com">info@emsvn.com</a></p>
+                        </div>       
+                    </div>
+                    <div id="google-map" class="col-sm-3">                        
+                        <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script><div style="overflow:hidden;height:500px;width:600px;"><div id="gmap_canvas" style="height:500px;width:600px;"><style>#gmap_canvas img{max-width:none!important;background:none!important}</style><a class="google-map-code" href="http://www.map-embed.com" id="get-map-data">embed google map</a></div><script type="text/javascript"> function init_map(){var myOptions = {zoom:15,center:new google.maps.LatLng(10.818838,106.6897265),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(10.818838, 106.6897265)});infowindow = new google.maps.InfoWindow({content:"<b>PS Media Co.Ltd</b><br/>624 L&ecirc; Quang &#272;&#7883;nh, G&ograve; V&#7845;p, H&#7891; Ch&iacute; Minh, Vi&#7879;t Nam<br/> " });google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>                        
+                    </div>                                 
+                    <div id="footer-introduction" class="col-sm-3">
+                       
+                    </div>                              
+                    <div id="footer-services" class="col-sm-3">
+                        <ul>
+                            <li class="clearfix">
+                                <a href="/trang-chu">Home</a>
+                            </li>
+                            <li class="clearfix">
+                                <a href="/gioi-thieu">Giới Thiệu</a>
+                            </li>
+                            <li class="clearfix">
+                                <a href="/tin-tuc">Tin Tức</a>
+                            </li>
+                            <li class="clearfix">
+                                <a href="/dich-vu">Dịch Vụ</a>
+                            </li>
+                            <li class="clearfix">
+                                <a href="/hoi-dap">Hỏi Đáp</a>
+                            </li>
+                            <li class="clearfix">
+                                <a href="/download">Download</a>
+                            </li>
+                            <li class="clearfix">
+                                <a href="/lien-he">Liên Hệ</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="copyright text-center">
+                        Copyright © 2016 <a>PSP Media</a>. All rights reserved
+                </div>
         </footer>
 
         {!! HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js') !!}
